815588411ab35986e353ebb5ed1b18e518cf6a5f
Luke Zoltan Kelley <lzkelley@gmail.com>
2025-01-31 14:21:09 -0800

    Complete version initially submitted to Elsevier and shared with colleagues.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pulsar Timing Arrays
+
+author: Luke Zoltan Kelley
+
+e-mail: LZKelley@berkeley.edu
+
+This is a pre-print of a chapter for the Encyclopedia of Astrophysics (edited by I. Mandel, section editor J. Andrews) to be published by Elsevier as a Reference Module.
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# LaTeX build using PDFLaTeX and BiBTeX
+# -------------------------------------
+#
+
+CWD=$(pwd)
+
+MY_PATH="./manuscript"
+FNAME="ptas-encyclopedia"
+OUTDIR="out"
+
+cd ${MY_PATH}
+
+TEX_FILE=${FNAME}.tex
+# OUTDIR=${OUT_DNAME}
+mkdir -p ${OUTDIR}
+
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error -output-directory=${OUTDIR} ${TEX_FILE}
+bibtex ${OUTDIR}/${FNAME}.aux
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error -output-directory=${OUTDIR} ${TEX_FILE}
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error -output-directory=${OUTDIR} ${TEX_FILE}
+
+cp ${OUTDIR}/${FNAME}.pdf .
+cp ${OUTDIR}/${FNAME}.pdf ${CWD}
+
+cd -
